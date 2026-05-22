82dd0a8c8c985c9201a0eaafbb68d0c5b20aa381
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-23 00:10:51 +0530

    Added Java program-3

---
+++ prog3.java
@@ -0,0 +1,13 @@
+public class prog3 {
+    public static void main(String[] args){
+        float a=12.3f;
+        float b=14.9f;
+        float add=a+b;
+        float sub=a-b;
+        float mul=a*b;
+        float div=a/b;
+        System.out.println(add+" "+sub+" "+mul+" "+div+" ");
+
+    }
+    
+}
