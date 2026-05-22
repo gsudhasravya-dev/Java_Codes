0670fba51151a2ba9d485e8f0952421b0c7e24cb
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-23 00:00:06 +0530

    Added Java programs

---
+++ prog1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class prog1{
+    public static void main(String[] args){
+        int a=10;
+        float b=12.5f;
+        boolean x=true;
+        String name="Sravya";
+        System.out.println(a+" "+b+" "+x+" "+name);
+    }
+}
