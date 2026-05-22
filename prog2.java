924e208dd4bd0feebf9f4818a0018dd6f2ab28d5
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-23 00:07:39 +0530

    Added Java program-2

---
+++ prog2.java
@@ -0,0 +1,11 @@
+public class prog2 {
+    public static void main(String[] args){
+        int a=10;
+        int b=45;
+        int add=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+        System.out.println(add+" "+sub+" "+mul+" "+div+" ");
+    }
+}
