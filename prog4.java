75b8be6470554fa858b53fe78146787e047acc5e
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-23 18:58:23 +0530

    Added Java program-3

---
+++ prog4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class prog4{
+    public static void main(String[] args){
+        String fname="Sudha";
+        String lname="Sravya";
+        System.out.println(fname+" " +lname);
+    }
+}
