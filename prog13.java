fe2514d42ba92e98d117f33ca20fcd963fbed0f0
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-25 23:19:06 +0530

    Added Java program-13

---
+++ prog13.java
@@ -0,0 +1,11 @@
+public class prog13 {
+    static void areaTri(int base, int height){
+        double area=0.5*base*height;
+        System.out.println(area);
+    }
+
+    public static void main(String[] args){
+        areaTri(23,56);
+    }
+    
+}
