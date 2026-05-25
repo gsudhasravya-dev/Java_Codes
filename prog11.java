c5d02dc6e6b130767a28bbb9b84a92bc762a0251
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-25 23:11:30 +0530

    Added Java program-11

--- prog11.java
+++ prog11.java
@@ -1,18 +1,17 @@
 import java.util.*;
 
-public class prog11 {
-    static void calculateArea(float length,float breadth){
-        float area=2*(length+breadth);
-        System.out.println("area is:"+ area);
+public class prog11{
+    void checkArea(double r){
+        float pi= 3.14f;
+        double area=pi*r*r;
+        System.out.println(area);
     }
 
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter length: ");
-        float length=sc.nextFloat();
-        System.out.println("Enter breadth: ");
-        float breadth=sc.nextFloat();
-        calculateArea(length, breadth);
+        prog11 r=new prog11();
+        r.checkArea(22.3);
+
     }
-    
+
+
 }
