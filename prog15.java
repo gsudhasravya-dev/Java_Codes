430d5a8494f5897d5ab8068788f3acab893c7677
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-25 23:27:06 +0530

    Added Java program-15

---
+++ prog15.java
@@ -0,0 +1,11 @@
+public class prog15 {
+    static void fahTocel(double fah){
+        double cel=(5f/9)*(fah-32);
+        System.out.println(cel);
+    }
+
+    public static void main(String[] args){
+        fahTocel(120.2);
+    }
+    
+}
