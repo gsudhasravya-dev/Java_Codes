5970d93bd5472767cd78d6d1dbb62ed75d6f0d5a
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-25 23:16:16 +0530

    Added Java program-12

---
+++ prog12.java
@@ -0,0 +1,11 @@
+public class prog12 {
+    static void tempconverter(double cel){
+        double fah=((9/5)*cel)+32;
+        System.out.println(fah);
+    }
+
+    public static void main(String[] args){
+        tempconverter(23.3);
+    }
+    
+}
