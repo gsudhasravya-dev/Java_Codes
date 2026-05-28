63208af0948fdb1f9bb76595cd0afe23e62b679a
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-28 22:16:47 +0530

    Added Java program-26

---
+++ proj26.java
@@ -0,0 +1,10 @@
+public class proj26 {
+    public static void main(String[] args) {
+        int num = 100;
+        double value = num;   // int → double
+
+        System.out.println(value);
+    }
+}
+    
+
