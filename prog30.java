71f8b0ba634b563ef6f3f54c027159c80319eb99
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-29 23:28:15 +0530

    Added Java program-30

---
+++ prog30.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class prog30{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your character:");
+        char ch = sc.next().charAt(1);
+        System.out.println(ch);
+    }
+
+}
