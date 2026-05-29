c76daca3fd65032ed7a6d792b40a0c6a305e9f21
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-29 23:20:46 +0530

    Added Java program-27

---
+++ prog27.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class prog27{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number:");
+        int num=sc.nextInt();
+        System.out.println(num);
+    }
+
+}
