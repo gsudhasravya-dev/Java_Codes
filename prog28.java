2cee5b2e49ea1e7c81e176f746f47920c8e09904
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-29 23:21:14 +0530

    Added Java program-28

---
+++ prog28.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class prog28{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any word:");
+        String word=sc.nextLine();
+        System.out.println(word);
+    }
+
+}
