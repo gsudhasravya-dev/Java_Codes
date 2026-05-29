c762d4a3f1da589d1a4fa19eaf51d18ceafe155c
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-29 23:30:26 +0530

    Added Java program-31

---
+++ prog31.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class prog31{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any sentence:");
+        String sent=sc.nextLine();
+        System.out.println(sent);
+    }
+
+}
