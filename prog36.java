dfb3790a0e22e8cceb2f66d33d015fbb8c69c3ca
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-30 14:35:59 +0530

    Added Java program-36

---
+++ prog36.java
@@ -0,0 +1,9 @@
+public class prog36{
+    public static void main(String[] args){
+        int num=Integer.MAX_VALUE;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+
+    }
+}
