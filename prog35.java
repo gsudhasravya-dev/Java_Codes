eef077d730f4a666ebbdf54378771b06c1761a08
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-30 14:26:36 +0530

    Added Java program-35

---
+++ prog35.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class prog35 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String word=sc.nextLine();
+        char ch=word.charAt(0);
+        System.out.println("Ascii value of 1st letter of word is:"+(int)ch);
+    }
+    
+}
