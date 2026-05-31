2c8b93a4e0965b28984fffcca2e4db2590683851
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-05-31 23:10:17 +0530

    Added Java program-38

---
+++ prog38.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class prog38 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 3 numbers:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
