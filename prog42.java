ffdbcbe3c81d8f054aeda7ad9d5c6c2b05ab6a55
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-06-01 23:48:20 +0530

    Added Java program-42

---
+++ prog42.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class prog42 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        sc.nextLine();
+        String word=sc.nextLine();
+        float numeric=sc.nextFloat();
+        System.out.println(num+" "+word+" "+numeric);
+    }
+    
+}
