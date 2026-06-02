4594a53f9b0f3629a5678b178efcd1ba64217da3
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-06-02 23:14:19 +0530

    Added Java program-45

---
+++ prog45.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class prog45 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        String regno=sc.nextLine();
+        String dept=sc.nextLine();
+        System.out.println("Name:"+name);
+        System.out.println("Reg no.:"+regno.toUpperCase());
+        System.out.println("Department:"+dept.toUpperCase());
+
+    }
+    
+}
