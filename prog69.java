62105c770c6d2c3298d1a2a120cf255ac2c72bf9
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-06-06 17:28:35 +0530

    Added Java program-69

---
+++ prog69.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class prog69{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int sum=0;
+        while(num>0){
+            int digit=num%10;
+            sum+=digit;
+            num/=10;
+        }
+        System.out.println("sum:"+sum);
+    }
+}
